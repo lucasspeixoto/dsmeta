680de4d2afa32994fedad6ac8274820fc5cbb7ab
Lucas Peixoto <lspeixotodev@gmail.com>
2022-11-15 08:49:55 -0300

    Config prettier, eslint and styled components theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import { useTheme } from "./hooks/useTheme";
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return <h1>App Component</h1>;
+
+
+    
+  const { theme } = useTheme();
+
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+
+        <NotificationButton></NotificationButton>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function NotificationButton() {
+  return (
+    <>
+      <button className='notification-button' type='button'>
+        Notificar
+      </button>
+    </>
+  );
+}
+
+export default NotificationButton;
