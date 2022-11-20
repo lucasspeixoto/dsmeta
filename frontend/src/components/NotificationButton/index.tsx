65330ce83dcfeec318aa413315c0ecc3ef52ad64
Lucas Peixoto <lspeixotodev@gmail.com>
2022-11-19 22:36:31 -0300

    Include sms send toast notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,9 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import icon from '@assets/images/notification-icon.svg';
 import axios from 'axios';
 import React, { ButtonHTMLAttributes } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import { getNotificationRoute } from 'utils/endpoints';
 import { BASE_URL } from 'utils/request';
 
@@ -11,16 +14,31 @@ type INotificationButton = ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 const NotificationButton: React.FC<INotificationButton> = ({ ...rest }) => {
+  const showNotificationConfirmationMessage = () => {
+    toast.info('SMS enviado!', {
+      position: toast.POSITION.TOP_RIGHT,
+      className: 'toast-message',
+      pauseOnHover: false,
+      progress: undefined,
+      hideProgressBar: true,
+    });
+  };
+
   const sendSaleNotificationHandler = () => {
     const endpoint = getNotificationRoute(rest.saleid);
-    axios.get(`${BASE_URL}/${endpoint}`);
+    axios
+      .get(`${BASE_URL}/${endpoint}`)
+      .then(() => showNotificationConfirmationMessage());
   };
   return (
-    <Container>
-      <button {...rest} onClick={sendSaleNotificationHandler}>
-        <img src={icon} alt="Notificar" />
-      </button>
-    </Container>
+    <React.Fragment>
+      <Container>
+        <button {...rest} onClick={sendSaleNotificationHandler}>
+          <img src={icon} alt="Notificar" />
+        </button>
+      </Container>
+      <ToastContainer />
+    </React.Fragment>
   );
 };
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 
+window.React = React;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
