0b616849590fd64b98ce7a34e96342b920652ce5
Lucas Peixoto <lspeixotodev@gmail.com>
2022-11-20 06:43:21 -0300

    Rename envirnments files

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,2 @@
-export const localUrl = 'http://localhost:8080';
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? localUrl;
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:8080';
